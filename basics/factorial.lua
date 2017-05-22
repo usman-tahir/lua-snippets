60420a7e8ddc4ed96bbf55ca6f19c8d693236c07
Usman Tahir <tahir.usman.ali94@outlook.com>
2017-05-21 23:44:57 -0400

    added a README and some basic snippets

---
+++ basics/factorial.lua
@@ -0,0 +1,12 @@
+
+function factorial(n)
+  if n == 0 then
+    return 1
+  else
+    return n * factorial(n - 1)
+  end
+end
+
+print("Enter a number: ")
+a = io.read("*number")
+print(factorial(a))
